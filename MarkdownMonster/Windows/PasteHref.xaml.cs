790e3e16baf0f865950c29605a17efbb53d841e5
Rick Strahl <rstrahl@west-wind.com>
2016-05-19 01:29:24 -0700

    Automatically pick up URLs from clipboard in the link dialog.

--- MarkdownMonster/Windows/PasteHref.xaml.cs
+++ MarkdownMonster/Windows/PasteHref.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,34 +14,95 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
+using MarkdownMonster.Annotations;
 
 namespace MarkdownMonster.Windows
 {
     /// <summary>
     /// Interaction logic for PasteHref.xaml
     /// </summary>
-    public partial class PasteHref : MetroWindow
+    public partial class PasteHref : MetroWindow, INotifyPropertyChanged
     {
+        private string _link;
+        private string _linkText;
+        private bool _isExternal;
+        private string _htmlResult;
 
-        public string Link { get; set; }        
-        public string LinkText { get; set; }
-        public bool IsExternal { get; set; }
-        public string HtmlResult { get; set; }
+        public string Link
+        {
+            get { return _link; }
+            set
+            {
+                if (value == _link) return;
+                _link = value;
+                OnPropertyChanged(nameof(Link));
+            }
+        }
+
+        public string LinkText
+        {
+            get { return _linkText; }
+            set
+            {
+                if (value == _linkText) return;
+                _linkText = value;
+                OnPropertyChanged(nameof(LinkText));
+            }
+        }
+
+        public bool IsExternal
+        {
+            get { return _isExternal; }
+            set
+            {
+                if (value == _isExternal) return;
+                _isExternal = value;
+                OnPropertyChanged(nameof(IsExternal));
+            }
+        }
+
+        public string HtmlResult
+        {
+            get { return _htmlResult; }
+            set
+            {
+                if (value == _htmlResult) return;
+                _htmlResult = value;
+                OnPropertyChanged(nameof(HtmlResult));
+            }
+        }
 
 
         public PasteHref()
         {
+            
             InitializeComponent();
 
             DataContext = this;
             mmApp.SetThemeWindowOverride(this);
 
             Loaded += PasteHref_Loaded;
-
+            
         }
 
         private void PasteHref_Loaded(object sender, RoutedEventArgs e)
         {
+                        if (string.IsNullOrEmpty(Link))
+            {
+                if (LinkText != null && LinkText.Contains("://") && !LinkText.Contains("\n"))
+                    Link = LinkText;
+                else
+                {
+                    string clipText = Clipboard.GetText();
+                    if (clipText != null && clipText.Contains("://") && !LinkText.Contains("\n"))
+                        Link = clipText;
+                }
+            }
+
+            if (string.IsNullOrEmpty(LinkText) && !string.IsNullOrEmpty(Link))
+                LinkText = Link;
+
+
             this.TextLink.Focus();
         }
 
@@ -53,6 +116,14 @@ namespace MarkdownMonster.Windows
             }
 
             Close();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
