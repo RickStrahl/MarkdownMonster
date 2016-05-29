4f060a0844d063dc3aa3bbf49e8e01bcfd8ef5c7
Rick Strahl <rstrahl@west-wind.com>
2016-05-28 22:52:27 -0700

    Customize the MahApps menu styling to use dark borders. This is a pain as brushes can't be overridden without copying the styles. And still can't get context menus but this is much better than the stark white borders and more in line with Visual Studio 2015.

--- AddIns/WebLogAddin/WebLogAddin.cs
+++ AddIns/WebLogAddin/WebLogAddin.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using FontAwesome.WPF;
 using HtmlAgilityPack;
 using JoeBlogs;
