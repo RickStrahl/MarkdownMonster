5d2dc19e9cbb5dec459eaffe18902ec7c0a6105f
Rick Strahl <rstrahl@west-wind.com>
2016-05-30 02:09:11 -0700

    Add copyright notices

--- MarkdownMonster/MainWindow.xaml.cs
+++ MarkdownMonster/MainWindow.xaml.cs
@@ -1,13 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
+﻿#region License
+/*
+ **************************************************************
+ *  Author: Rick Strahl 
+ *          © West Wind Technologies, 2016
+ *          http://www.west-wind.com/
+ * 
+ * Created: 04/28/2016
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ **************************************************************  
+*/
+#endregion
+
+using System;
 using System.Linq;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
-using System.IO.Pipes;
 using System.Runtime.InteropServices;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -15,8 +45,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Threading;
 using Dragablz;
-using Dragablz.Core;
-using Dragablz.Dockablz;
 using FontAwesome.WPF;
 using MahApps.Metro.Controls;
 using MarkdownMonster.AddIns;
@@ -26,6 +54,7 @@ using Westwind.Utilities;
 
 namespace MarkdownMonster
 {
+  
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
