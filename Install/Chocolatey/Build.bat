a3a01d422d33bf3a786ea6a034fd61c9c4985dc4
Rick Strahl <rstrahl@west-wind.com>
2016-09-15 22:12:52 -0700

    Update chocolatey install - go back to hosted file download from GitHub releases folder.

--- Install/Chocolatey/Build.bat
+++ Install/Chocolatey/Build.bat
@@ -1,5 +1,5 @@
 cd %~dp0
 call choco pack
-call choco install "MarkdownMonster" -fdv  -s "C:\projects2010\MarkdownMonster\Install\Chocolatey"
-
+call choco install "MarkdownMonster" -fdv  -s "C:\projects2010\MarkdownMonster\Install\Chocolatey\New\markdownmonster"
+ 
 pause
