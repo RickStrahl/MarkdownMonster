ad09c694ba34309345a074e5e8564fbb956a0b65
Rick Strahl <rstrahl@west-wind.com>
2016-09-16 20:13:01 -0700

    Update chocolatey to web based download rather than embedded file. Update readme

---
+++ Install/Chocolatey/Old/CopyExe.bat
@@ -0,0 +1,2 @@
+cd %~dp0
+copy ..\Builds\CurrentRelease\MarkdownMonsterSetup.exe .\tools
--- Install/Chocolatey/tools/chocolateyInstall.ps1
+++ Install/Chocolatey/tools/chocolateyInstall.ps1
@@ -4,4 +4,4 @@ $url = 'https://github.com/RickStrahl/MarkdownMonster/raw/master/Install/Builds/
 $silentArgs = '/q'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$url"  -validExitCodes -checksum='BC06913C55F15D184669F21019864F9321419F483F1ACE57E0553858D25DC70A' -checksumType='sha256' $validExitCodes
