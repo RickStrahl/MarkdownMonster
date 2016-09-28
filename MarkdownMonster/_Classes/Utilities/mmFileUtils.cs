cf6d8edbb0c62430160623b142e9544f149150fa
Rick Strahl <rstrahl@west-wind.com>
2016-09-27 18:45:47 -0700

    Fix launching of files using relative paths.

---
+++ MarkdownMonster/_Classes/Utilities/mmFileUtils.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+namespace MarkdownMonster 
+{
+    /// <summary>
+    /// Internal File Utilities class
+    /// </summary>
+    public class mmFileUtils
+    {
+        /// <summary>
+        /// Method checks for existance of full filename and tries
+        /// to check for file in the initial startup folder.
+        /// </summary>
+        /// <param name="file">Name of file - fully qualified or local folder file</param>
+        /// <returns>filename or null if file doesn't exist</returns>
+        public static string FixupDocumentFilename(string file)
+        {
+            if (File.Exists(file))
+                return file;
+
+            var newFile = Path.Combine(App.initialStartDirectory, file);
+            if (File.Exists(newFile))
+                return newFile;
+
+            return null;
+        }
+
+    }
+}
