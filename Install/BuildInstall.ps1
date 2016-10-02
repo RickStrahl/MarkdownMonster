6b4deb5ae853df1ff92e430a255d3df68d9dd709
Rick Strahl <rstrahl@west-wind.com>
2016-10-02 12:33:03 -1000

    More installation cleanup

--- Install/BuildInstall.ps1
+++ Install/BuildInstall.ps1
@@ -3,7 +3,7 @@
 
 "Running Inno Setup"
 # & "C:\Program Files\InstallMate 9\BinX64\tin.exe" /build:all "West Wind Markdown Monster.im9" | Out-null
-& "C:\Program Files (x86)\Inno Setup 5\compil32.exe" /cc "MarkdownMonster.iss" | Out-null
+& "C:\Program Files (x86)\Inno Setup 5\iscc.exe" "MarkdownMonster.iss" 
 
 "Zipping up setup file..."
 7z a -tzip "$PSScriptRoot\Builds\CurrentRelease\MarkdownMonsterSetup.zip" ".\Builds\CurrentRelease\MarkdownMonsterSetup.exe"
--- Install/Chocolatey/tools/chocolateyInstall.ps1
+++ Install/Chocolatey/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'markdownmonster'
 $fileType = 'exe'
 $url = 'https://github.com/RickStrahl/MarkdownMonster/raw/master/Install/Builds/PreRelease/MarkdownMonsterSetup-0.49.exe'
-$silentArgs = '/q'
+$silentArgs = '/SILENT'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$url"  -validExitCodes  $validExitCodes  -checksum "314E6B87B6C5D1B5010492CDD47FA8E0369242338690F289B868BA4FCF1E6E9E" -checksumType "sha256"
