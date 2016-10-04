dad9bfc565776e65cfb9b7e885da6a8b45081586
Rick Strahl <rstrahl@west-wind.com>
2016-10-03 18:53:00 -1000

    Add certificate based code signing to build process

--- Install/BuildInstall.ps1
+++ Install/BuildInstall.ps1
@@ -1,5 +1,8 @@
+﻿& "$PSScriptRoot\CopyFiles.ps1"
 
-& "$PSScriptRoot\CopyFiles.ps1"
+
+& "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin\signtool.exe" sign /v /n "West Wind Technologies" /sm /s MY /t "http://timestamp.verisign.com/scripts/timstamp.dll"  ".\Distribution\MarkdownMonster.exe"
+
 
 # Installmate setup
 # & "C:\Program Files\InstallMate 9\BinX64\tin.exe" /build:all "West Wind Markdown Monster.im9" | Out-null
@@ -7,8 +10,8 @@
 "Running Inno Setup..."
 & "C:\Program Files (x86)\Inno Setup 5\iscc.exe" "MarkdownMonster.iss" 
 
+& "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin\signtool.exe" sign /v /n "West Wind Technologies" /sm /s MY /t "http://timestamp.verisign.com/scripts/timstamp.dll"  ".\Builds\CurrentRelease\MarkdownMonsterSetup.exe"
 
-& "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin\signtool.exe" sign /v /t http://timestamp.comodoca.com/authenticode  /n "West Wind Technologies" ".\Builds\CurrentRelease\MarkdownMonsterSetup.exe"
 
 "Zipping up setup file..."
 7z a -tzip "$PSScriptRoot\Builds\CurrentRelease\MarkdownMonsterSetup.zip" ".\Builds\CurrentRelease\MarkdownMonsterSetup.exe"
