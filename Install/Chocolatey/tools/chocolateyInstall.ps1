52ffcb4dc76e0c06557cdc499e260135c469e973
Rick Strahl <rstrahl@west-wind.com>
2016-10-12 13:30:01 -1000

    Update for Chocolatey v0.52

--- Install/Chocolatey/tools/chocolateyInstall.ps1
+++ Install/Chocolatey/tools/chocolateyInstall.ps1
@@ -4,4 +4,5 @@ $url = 'https://github.com/RickStrahl/MarkdownMonster/raw/master/Install/Builds/
 $silentArgs = '/SILENT'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$url"  -validExitCodes  $validExitCodes  -checksum "E4C9E80BA0EABDD02D595B07F0824B48261F59FB6A2BF8AD2E114756502B568C" -checksumType "sha256"
+Install-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$url"  -validExitCodes  $validExitCodes  -checksum "69967996AF42AB5BCCB487095CF3EAF0D7E1CFA6B88CA9099E49F322A7EDCD93" -checksumType "sha256"
+
