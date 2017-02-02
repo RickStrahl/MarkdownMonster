2106ea3dee450eb89ce2f76f8115d040a53b2995
Rick Strahl <rstrahl@west-wind.com>
2017-02-02 00:43:51 -1000

    v0.4

--- MarkdownMonster/Properties/AssemblyInfo.cs
+++ MarkdownMonster/Properties/AssemblyInfo.cs
@@ -54,4 +54,4 @@ using System.Windows.Media;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.23")]
+[assembly: AssemblyVersion("1.1.24")]
