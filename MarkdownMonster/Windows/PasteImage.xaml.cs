710d079acfe60dd1bb84c0856e213a7c24b33262
Rick Strahl <rstrahl@west-wind.com>
2017-02-10 17:11:33 -1000

    Update Paste Image dialog to support clipboard images. If image is on clipboard it'll be shown. Button to explicitly add images from clipboard. Clipboard images are saved to disk first and filename pasted into dialog text.

--- MarkdownMonster/Windows/PasteImage.xaml.cs
+++ MarkdownMonster/Windows/PasteImage.xaml.cs
@@ -49,6 +49,42 @@ namespace MarkdownMonster.Windows
         public string MarkdownFile { get; set; }
 
 
+
+        public CommandBase PasteCommand
+        {
+            get { return _pasteCommand; }
+            set
+            {
+                _pasteCommand = value;
+                OnPropertyChanged(nameof(PasteCommand));
+            }
+        }
+
+        private CommandBase _pasteCommand;
+
+        public bool IsFileImage
+        {
+            get { return !_isMemoryImage; }
+        }
+
+        public bool IsMemoryImage
+        {
+            get { return _isMemoryImage; }
+            set
+            {
+                if (value == _isMemoryImage) return;
+                _isMemoryImage = value;
+                OnPropertyChanged(nameof(IsMemoryImage));
+                OnPropertyChanged(nameof(IsFileImage));
+            }
+        }
+
+        private bool _isMemoryImage;
+
+        MarkdownDocumentEditor Editor { get; set; }
+        MarkdownDocument Document { get; set; }
+
+
         public PasteImage()
         {
             InitializeComponent();
@@ -58,12 +94,32 @@ namespace MarkdownMonster.Windows
 
             Loaded += PasteImage_Loaded;
             SizeChanged += PasteImage_SizeChanged;
+            Activated += PasteImage_Activated;
+        }
+
+        private void PasteImage_Activated(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Image) && Clipboard.ContainsImage())            
+                PasteImageFromClipboard();            
         }
 
         private void PasteImage_Loaded(object sender, RoutedEventArgs e)
         {
+            var window = this.Owner as MainWindow;
+            Editor = window.Model.ActiveEditor;
+            Document = window.Model.ActiveDocument;
+
+            PasteCommand = new CommandBase((s, args) =>
+            {
+                MessageBox.Show("PasteCommand");
+            });
+
             TextImage.Focus();
-            if (Clipboard.ContainsText())
+            if (Clipboard.ContainsImage())
+            {
+                Button_PasteImage(null, null);
+            }
+            else if (Clipboard.ContainsText())
             {
                 string clip = Clipboard.GetText().ToLower();
                 if ((clip.StartsWith("http://") || clip.StartsWith("https://")) &&
@@ -73,44 +129,6 @@ namespace MarkdownMonster.Windows
                     SetImagePreview(clip);
                 }
             }
-        }
-
-        private void PasteImage_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            var image = ImagePreview.Source as BitmapSource;
-            if (image == null)
-                return;
-
-            if (image.Width < Width - 20 && image.Height < PageGrid.RowDefinitions[1].ActualHeight)
-                ImagePreview.Stretch = Stretch.None;
-            else
-                ImagePreview.Stretch = Stretch.Uniform;
-        }
-
-        private void TextImage_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrEmpty(TextImage.Text))
-            {
-                ImagePreview.Source = null;
-                return;
-            }
-                
-            string href = TextImage.Text.ToLower();
-            if (href.StartsWith("http://") || href.StartsWith("https://"))
-            {
-                SetImagePreview(href);                
-            }
-
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender == ButtonCancel)
-                DialogResult = false;
-            else
-                DialogResult = true;
-
-            Close();
         }
 
         private void SelectLocalImageFile_Click(object sender, RoutedEventArgs e)
@@ -211,8 +229,7 @@ namespace MarkdownMonster.Windows
                             {
                                 mmApp.Log($"Failed to get relative path.\r\nFile: {sd.FileName}, Path: {mdPath}", ex);
                             }
-                            if (!relPath.StartsWith("..\\"))
-                                Image = relPath;
+                            Image = relPath;
                         }
                     }
                 }
@@ -221,20 +238,158 @@ namespace MarkdownMonster.Windows
             if (Image.Contains(":\\"))
                 Image = "file:///" + Image;
 
-            SetImagePreview("file:///" + fd.FileName);            
+            SetImagePreview("file:///" + fd.FileName);
+
+            IsMemoryImage = false;
 
             mmApp.Configuration.LastImageFolder = Path.GetDirectoryName(fd.FileName);
             TextImageText.Focus();
+            
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        private void TextImage_LostFocus(object sender, RoutedEventArgs e)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            
+            if (IsMemoryImage && string.IsNullOrEmpty(TextImage.Text))
+            {
+                ImagePreview.Source = null;                
+                return;
+            }
+
+            IsMemoryImage = false;
+
+            string href = TextImage.Text.ToLower();
+            if (href.StartsWith("http://") || href.StartsWith("https://"))
+            {
+                SetImagePreview(href);
+            }            
         }
 
+        #region Main Buttons
 
+        private void Button_SaveImage(object sender, RoutedEventArgs e)
+        {
+            string imagePath = null;
+            using (var bitMap = WindowUtilities.BitmapSourceToBitmap(ImagePreview.Source as BitmapSource))
+            {
+                imagePath = AddinManager.Current.RaiseOnSaveImage(bitMap);
+            }
+
+            if (!string.IsNullOrEmpty(imagePath))
+            {
+                TextImage.Text = imagePath;
+                IsMemoryImage = false;
+                return;
+            }
+
+
+            string initialFolder = null;
+            if (!string.IsNullOrEmpty(Document.Filename) && Document.Filename != "untitled")
+                initialFolder = Path.GetDirectoryName(Document.Filename);
+
+            var sd = new SaveFileDialog
+            {
+                Filter = "Image files (*.png;*.jpg;*.gif;)|*.png;*.jpg;*.jpeg;*.gif|All Files (*.*)|*.*",
+                FilterIndex = 1,
+                Title = "Save Image from Clipboard as",
+                InitialDirectory = initialFolder,
+                CheckFileExists = false,
+                OverwritePrompt = true,
+                CheckPathExists = true,
+                RestoreDirectory = true
+            };
+            var result = sd.ShowDialog();
+            if (result != null && result.Value)
+            {
+                imagePath = sd.FileName;
+
+                try
+                {
+                    var ext = Path.GetExtension(imagePath)?.ToLower();
+
+                    using (var fileStream = new FileStream(imagePath, FileMode.Create))
+                    {
+                        BitmapEncoder encoder = null;
+                        if (ext == ".png")
+                            encoder = new PngBitmapEncoder();
+                        else if (ext == ".jpg")
+                            encoder = new JpegBitmapEncoder();
+                        else if (ext == ".gif")
+                            encoder = new GifBitmapEncoder();
+
+                        encoder.Frames.Add(BitmapFrame.Create(ImagePreview.Source as BitmapSource));
+                        encoder.Save(fileStream);
+
+                        if (ext == ".png")
+                            mmFileUtils.OptimizePngImage(sd.FileName, 5); // async
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Couldn't copy file to new location: \r\n" + ex.Message, mmApp.ApplicationName);
+                    return;
+                }
+
+                string relPath = Path.GetDirectoryName(sd.FileName);
+                if (initialFolder != null)
+                {
+                    try
+                    {
+                        relPath = FileUtils.GetRelativePath(sd.FileName, initialFolder);
+                    }
+                    catch (Exception ex)
+                    {
+                        mmApp.Log($"Failed to get relative path.\r\nFile: {sd.FileName}, Path: {imagePath}", ex);
+                    }
+                    imagePath = relPath;
+                }
+
+                if (imagePath.Contains(":\\"))
+                    imagePath = "file:///" + imagePath;
+
+                imagePath = imagePath.Replace("\\", "/");
+
+                this.Image = imagePath;
+                IsMemoryImage = false;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == ButtonCancel)
+                DialogResult = false;
+            else
+                DialogResult = true;
+
+            Close();
+        }
+
+        private void PasteImageFromClipboard()
+        {
+            SetImagePreview(Clipboard.GetImage());
+            IsMemoryImage = true;
+        }
+
+        private void Button_PasteImage(object sender, RoutedEventArgs e)
+        {
+            PasteImageFromClipboard();
+        }
+
+        #endregion
+
+        #region Image Display
+
+        private void PasteImage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var image = ImagePreview.Source as BitmapSource;
+            if (image == null)
+                return;
+
+            if (image.Width < Width - 20 && image.Height < PageGrid.RowDefinitions[1].ActualHeight)
+                ImagePreview.Stretch = Stretch.None;
+            else
+                ImagePreview.Stretch = Stretch.Uniform;
+        }
 
         private void SetImagePreview(string url)
         {
@@ -244,7 +399,7 @@ namespace MarkdownMonster.Windows
                 if (Height < 400)
                 {
                     Top -= 300;
-                    Left -= 100;                    
+                    Left -= 100;
                     Width = 800;
                     Height = 800;
                 }
@@ -256,5 +411,37 @@ namespace MarkdownMonster.Windows
             {
             }
         }
+
+        private void SetImagePreview(BitmapSource source)
+        {
+            try
+            {
+                ImagePreview.Source = source;
+                if (Height < 400)
+                {
+                    Top -= 300;
+                    Left -= 100;
+                    Width = 800;
+                    Height = 800;
+                }
+
+                WindowUtilities.DoEvents();
+                PasteImage_SizeChanged(this, null);
+            }
+            catch
+            {
+            }
+        }
+
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        
     }
 }
