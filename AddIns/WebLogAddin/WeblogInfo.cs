e485d8868e2f834da69305237059df9c83401457
Rick Strahl <rstrahl@west-wind.com>
2017-02-17 17:36:37 -1000

    Fix addin project dependencies. Add removed package references back in.

--- AddIns/WebLogAddin/WeblogInfo.cs
+++ AddIns/WebLogAddin/WeblogInfo.cs
@@ -2,7 +2,6 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
 using MarkdownMonster.Annotations;
-using Newtonsoft.Json;
 using Westwind.Utilities;
 
 namespace WeblogAddin
