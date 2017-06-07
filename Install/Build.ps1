58b55ff2e07caeee9550a5f11dd31bf85c30b5b9
Rick Strahl <rstrahl@west-wind.com>
2017-06-07 00:39:12 -0700

    Remove extraneous install stuff and update output messages

--- Install/Build.ps1
+++ Install/Build.ps1
@@ -4,12 +4,10 @@
 & ".\signtool.exe" sign /v /n "West Wind Technologies" /sm /s MY /tr "http://timestamp.digicert.com" /td SHA256 /fd SHA256 ".\Distribution\MarkdownMonster.exe"
 & ".\signtool.exe" sign /v /n "West Wind Technologies" /sm /s MY /tr "http://timestamp.digicert.com" /td SHA256 /fd SHA256 ".\Distribution\mm.exe"
 
-# Installmate setup
-#& "C:\Program Files\InstallMate 9\BinX64\tin.exe" /build:all "West Wind Markdown Monster.im9" | Out-null
-
 "Running Inno Setup..."
 & "C:\Program Files (x86)\Inno Setup 5\iscc.exe" "MarkdownMonster.iss" 
 
+"Signing the main EXE..."
 & ".\signtool.exe" sign /v /n "West Wind Technologies" /sm  /tr "http://timestamp.digicert.com" /td SHA256 /fd SHA256 ".\Builds\CurrentRelease\MarkdownMonsterSetup.exe"
 
 copy ".\MarkdownMonsterPortable.md" ".\Distribution"
