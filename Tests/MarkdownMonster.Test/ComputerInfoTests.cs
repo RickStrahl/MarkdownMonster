6a8d3f4e248939ece388d70a9e161700cbd92c3e
Rick Strahl <rstrahl@west-wind.com>
2017-06-30 11:48:27 -0700

    Add tests for EnsureFileAssociation

---
+++ Tests/MarkdownMonster.Test/ComputerInfoTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarkdownMonster.Test
+{
+    [TestClass]
+    public class ComputerInfoTests
+    {
+        [TestMethod]
+        public void EnsureAssociationsTest()
+        {
+            ComputerInfo.EnsureAssociations();
+        }
+
+        [TestMethod]
+        public void EnsureAssociationsForceTest()
+        {
+            ComputerInfo.EnsureAssociations(true);
+        }
+    }
+}
