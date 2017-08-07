b15a1f7dbff561bdf0fba183494de9613a59dadd
Rick Strahl <rstrahl@west-wind.com>
2017-08-06 21:35:39 -0700

    v1.5.1

---
+++ Install/DesktopBridge/DesktopConverterNotes.md
@@ -0,0 +1,11 @@
+﻿### Windows Version
+10.0.15063 Build 15063
+
+### Location
+c:\temp\AppxBaseImages\BaseImage-15063-Update.wim
+
+* Download Image from Microsoft Site
+* Run As Administrator `Desktop App Converter` from Windows Menu
+* Set-ExecutionPolicy bypass
+
+
---
+++ Install/DesktopBridge/build.ps1
@@ -0,0 +1,15 @@
+#REM  Run from DesktopConverter Command Line
+DesktopAppConverter.exe `
+    -Destination C:\Temp\AppxBaseImages\MarkdownMonster `
+    -Installer ..\Builds\CurrentRelease\MarkdownMonsterSetup.exe `
+    -InstallerArguments "/VERYSILENT" `    
+    -PackageName "MarkdownMonster" `
+    -Publisher "CN=West Wind Technologies,C=US" `
+    -Version "1.5.1.3" `
+    -AppExecutable "MarkdownMonster.exe" `
+    -PackageDisplayName "Markdown Monster" `
+    -PackagePublisherDisplayName "West Wind Technologies" `
+    -AppDisplayName "Markdown Monster" `
+    -AppDescription "A rich Markdown Editor and Weblog Publisher" `
+    -AppFileTypes ".md" `
+    -MakeAppx -Verbose -sign -verify
