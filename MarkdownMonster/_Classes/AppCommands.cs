56c5b0b729270b3fe0aee028562ad4f68c392ae8
Rick Strahl <rstrahl@west-wind.com>
2017-09-23 13:27:21 -1000

    Add new AppComands object to hold application commmands.
    Under construction - eventually move all App commands to this structure and also most Button_Handler() event handlers.

---
+++ MarkdownMonster/_Classes/AppCommands.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarkdownMonster.AddIns;
+using MarkdownMonster.Windows;
+
+namespace MarkdownMonster
+{
+    public class AppCommands
+    {
+        AppModel Model;
+
+        public AppCommands(AppModel model)
+        {
+            Model = model;
+
+            // File Operations
+            Command_NewWeblogPost();
+
+
+            // Configuration 
+            Command_AddinManager();
+        }
+        
+        public CommandBase NewWeblogPostCommand { get; set; }
+
+        void Command_NewWeblogPost()
+        {
+            NewWeblogPostCommand = new CommandBase((parameter, command) =>
+            {
+                AddinManager.Current.RaiseOnNotifyAddin("newweblogpost", null);
+            });
+        }
+
+
+
+        public CommandBase AddinManagerCommand { get; set; }
+
+        void Command_AddinManager()
+        {
+            AddinManagerCommand = new CommandBase((parameter, command) =>
+            {
+                var form = new AddinManagerWindow
+                {
+                    Owner = Model.Window
+                };
+                form.Show();
+            });
+        }
+
+
+    }
+}
