b25fe356582147660fb7d75ea4c453a02fa6a82a
Rick Strahl <rstrahl@west-wind.com>
2017-11-05 22:00:14 -1000

    Clean up registration form.

--- MarkdownMonster/Windows/RegistrationForm.xaml.cs
+++ MarkdownMonster/Windows/RegistrationForm.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using Westwind.Utilities;
 
 namespace MarkdownMonster.Windows
@@ -62,14 +52,10 @@ namespace MarkdownMonster.Windows
                 UnlockKey.UnRegister();
             }
         }
-        private void Exit_KeyDown(object sender, KeyEventArgs e)
-        {
-            this.Close();
-        }
 
         private void Exit_Click(object sender, MouseButtonEventArgs e)
         {
-            this.Close();
+            Close();
         }
 
         private void Purchase_Click(object sender, RoutedEventArgs e)
