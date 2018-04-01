64334d68fb1fc150d538e32eb9e9d1cf0522cb6d
Rick Strahl <rstrahl@west-wind.com>
2018-04-01 00:50:30 -1000

    Update TextBox and ComboBox styling for more consistent dialog input sizing.

--- Install/Build.ps1
+++ Install/Build.ps1
@@ -19,7 +19,7 @@ del ".\Builds\CurrentRelease\MarkdownMonsterSetup.zip"
 "Zipping up portable setup file..."
 del ".\Builds\CurrentRelease\MarkdownMonsterPortable.zip"
 .\7z a -tzip -r ".\Builds\CurrentRelease\MarkdownMonsterPortable.zip" ".\Distribution\*.*"
-.\7z a -tzip ".\Builds\CurrentReleasbuie\MarkdownMonsterPortable.zip" ".\MarkdownMonsterPortable.md"
+.\7z a -tzip ".\Builds\CurrentRelease\MarkdownMonsterPortable.zip" ".\MarkdownMonsterPortable.md"
 
 
 $version = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$PSScriptRoot\builds\currentrelease\MarkdownMonsterSetup.exe").FileVersion
