22b203b59df80dd6f936fd7ebc17081681553ee5
Rick Strahl <rstrahl@west-wind.com>
2018-08-14 22:59:03 -0700

    Fix hash clicks in the preview editor that requires manual navigation via code.

--- MarkdownMonster/PreviewThemes/scripts/preview.js
+++ MarkdownMonster/PreviewThemes/scripts/preview.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
             }
                        
             if (hash) {                
-                var sel = hash + "[name='" + hash.substr(1) + "'],#"+ hash.substr(1);                                          
+                var sel = "[name='" + hash.substr(1) + "'],#"+ hash.substr(1);                                          
                 var $el = $(sel);                
                 $("html").scrollTop($el.offset().top - 100);
                 return false;
