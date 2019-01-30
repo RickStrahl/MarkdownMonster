b321d9b82464c0d667d010c836ff873cad6a35e5
Rick Strahl <rstrahl@west-wind.com>
2019-01-30 00:58:35 -1000

    Fix for removing unsed Roslyn dependency files in the distribution copying.

--- Install/CopyFiles.ps1
+++ Install/CopyFiles.ps1
@@ -16,6 +16,11 @@ Copy-Item ${cur}\license.md ${target}\license.md
 Remove-Item ${target}\*.vshost.*
 Remove-Item ${target}\*.xml
 
+Remove-Item ${target}\Addins\Snippets\roslyn -Recurse -Force
+Remove-Item ${target}\roslyn\Microsoft.CodeAnalysis.VisualBasic.dll
+Remove-Item ${target}\roslyn\Microsoft.DiaSymReader.Native.amd64.dll
+Remove-Item ${target}\roslyn\Microsoft.DiaSymReader.Native.x86.dll
+
 # Want to ship main PDB but not any others
 Rename-Item ${target}\markdownmonster.pdb ${target}\markdownmonster.TPDB
 Remove-Item ${target}\*.pdb
