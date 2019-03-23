1f4ae0827041ecdb7f19039807d6c2c48036dd48
Rick Strahl <rstrahl@west-wind.com>
2019-03-23 13:32:32 -1000

    1.16.3

--- Install/MarkdownMonsterLaunchMM/mm/Properties/AssemblyInfo.cs
+++ Install/MarkdownMonsterLaunchMM/mm/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("mm")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Markdown Monster Quick Launcher")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("mm")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("West Wind Technologies")]
+[assembly: AssemblyProduct("Markdown Monster")]
+[assembly: AssemblyCopyright("Copyright © West Wind Technologies, 2017-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.16.0.0")]
+[assembly: AssemblyFileVersion("1.16.0.0")]
