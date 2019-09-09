8e8be08896c4c78a09f85b012738832863ea0cea
Rick Strahl <rstrahl@west-wind.com>
2019-09-09 13:09:09 -0700

    Fix Preview issues with templates that are not using the highlightjsBadge yet.

--- MarkdownMonster/PreviewThemes/scripts/preview.js
+++ MarkdownMonster/PreviewThemes/scripts/preview.js
@@ -204,7 +204,8 @@ function highlightCode(lineno) {
     }
 
     // add the code snippet syntax and code copying
-    if (highlightJsBadge)  highlightJsBadge();
+
+    if (window.highlightJsBadge)  window.highlightJsBadge();
 }
 
 // this works, but async updates of code blocks is too jumpy
