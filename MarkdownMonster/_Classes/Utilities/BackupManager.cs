acc8c2f2467abf33903d6a00c196371410829bce
rickstrahl <rstrahl@west-wind.com>
2020-05-23 11:15:31 -1000

    Add xml docs to Backup Manager.

--- MarkdownMonster/_Classes/Utilities/BackupManager.cs
+++ MarkdownMonster/_Classes/Utilities/BackupManager.cs
@@ -9,6 +9,12 @@ using Westwind.Utilities;
 
 namespace MarkdownMonster.Utilities
 {
+
+    /// <summary>
+    /// Allows you to back the Markdown Monster Common Folder that
+    /// contains all the Markdown Monster and Addin settings to
+    /// a folder or zip file.
+    /// </summary>
     public class BackupManager
     {
         AppModel Model { get;  }
@@ -18,6 +24,11 @@ namespace MarkdownMonster.Utilities
             Model = mmApp.Model;
         }
 
+        /// <summary>
+        /// Backs up the Markdown Monster Common folder to a zip file.
+        /// </summary>
+        /// <param name="outputZipFile"></param>
+        /// <returns></returns>
         public bool BackupToZip(string outputZipFile)
         {
             var id = "_" + DataUtils.GenerateUniqueId();
@@ -40,19 +51,35 @@ namespace MarkdownMonster.Utilities
             return true;
         }
 
+        /// <summary>
+        /// Backs up the Markdown Monster Common folder to a new folder
+        /// </summary>
+        /// <param name="outputFolder"></param>
+        /// <returns></returns>
         public bool BackupToFolder(string outputFolder)
         {
             var id = "_" + DataUtils.GenerateUniqueId();
-            outputFolder = Path.GetFullPath(outputFolder);
+            outputFolder = @"\\?\" + Path.GetFullPath(outputFolder);
             string common = Model.Configuration.CommonFolder;
+
+            try
+            {
+                if (!Directory.Exists(outputFolder))
+                    Directory.CreateDirectory(outputFolder);
+            }
+            catch
+            {
+                return false;
+            }
 
             FileUtils.CopyDirectory(common, outputFolder, deepCopy: true);
 
             FileUtils.DeleteFiles(outputFolder, "*Copy.*", recursive: true);
             FileUtils.DeleteFiles(outputFolder, "*Backup*.*", recursive: true);
             FileUtils.DeleteFiles(outputFolder, "*.bak", recursive: true);
-            
-            
+
+            if( Directory.Exists(Path.Combine(outputFolder,"backups") ))
+                Directory.Delete(Path.Combine(outputFolder, "backups"),true);
 
             return true;
         }
