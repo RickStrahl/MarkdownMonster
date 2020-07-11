d530c47558c9f10876e4d5de1571a8b3d5781dc8
rickstrahl <rstrahl@west-wind.com>
2020-07-10 22:59:23 -1000

    Fix small bug in command line parser.

--- mmcli/CommandLine/CommandLineParser.cs
+++ mmcli/CommandLine/CommandLineParser.cs
@@ -6,7 +6,7 @@ namespace mmcli.CommandLine
     /// <summary>
     /// Basic Command Line Parser class that can deal with simple
     /// switch based command line arguments
-    /// 
+    ///
     /// supports: FirstParm (first commandline argume
     /// -pString or -p"String"
     /// -f     switch/flag parameters
@@ -26,7 +26,7 @@ namespace mmcli.CommandLine
         public string CommandLine { get; set; }
 
         /// <summary>
-        /// The first argument (if any). Useful for a 
+        /// The first argument (if any). Useful for a
         /// command/action parameter
         /// </summary>
         public string FirstParameter { get; set; }
@@ -68,12 +68,12 @@ namespace mmcli.CommandLine
 
         /// <summary>
         /// Parses a string Parameter switch in the format of:
-        /// 
+        ///
         /// -p"c:\temp files\somefile.txt"
         /// -pc:\somefile.txt
-        /// 
+        ///
         /// Note no spaces are allowed between swich and value.
-        /// </summary>        
+        /// </summary>
         /// <param name="parm">parameter switch key</param>
         /// <param name="nonMatchingValue">Value to return if no match is found</param>
         /// <returns>Match or non-matching value</returns>
@@ -83,7 +83,13 @@ namespace mmcli.CommandLine
 
             if (at > -1)
             {
-                string rest = CommandLine.Substring(at + parm.Length).Trim();
+                string rest = CommandLine.Substring(at + parm.Length);
+
+                // next character needs to be a space
+                if (!rest.StartsWith(" "))
+                    return nonMatchingValue;
+
+				rest = rest.Trim();
 
                 if (rest.StartsWith("\""))
                 {
@@ -94,15 +100,9 @@ namespace mmcli.CommandLine
 
                     return rest.Substring(1, at - 1);
                 }
-                else if (rest == " ")
-                {
-                    // no spaces after parameters
-                    return CommandLine;
-                }
                 else
                 {
                     // read to next space
-                    rest = rest.Trim();
                     at = (rest + " ").IndexOf(' ');
                     string stringParm = rest.Substring(0, at);
                     if (!string.IsNullOrEmpty(stringParm))
