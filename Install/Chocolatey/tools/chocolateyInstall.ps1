fc1d6bb2fbf30592b7a80bb889fbda54bbdd1f8d
Rick Strahl <rstrahl@west-wind.com>
2021-03-31 18:41:53 -1000

    Project Housekeeping.

--- Install/Chocolatey/tools/chocolateyInstall.ps1
+++ Install/Chocolatey/tools/chocolateyInstall.ps1
@@ -5,4 +5,4 @@ $url = 'https://github.com/RickStrahl/MarkdownMonsterReleases/raw/master/v1.26/M
 $silentArgs = '/VERYSILENT'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$url"  -validExitCodes  $validExitCodes  -checksum "B93975C42B18CCE05D9FCF745D801D9F377F5EF09433B63E1875DA70FB9121A9" -checksumType "sha256"
+Install-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$url"  -validExitCodes  $validExitCodes  -checksum "0251BB21564854172BB9A3BB8672C4AECC57CE5BF568619316678530F9B39CBC" -checksumType "sha256"
